4efe98a881c6b4db4f71b7be8c074ab5ee560f01
Dan Isaacman <106420953+DIsaacman@users.noreply.github.com>
2022-07-28 20:20:26 +1000

    SQL_order

--- char.py
+++ char.py
@@ -1,7 +1,10 @@
 # Initiate dcitionary of lists with syntax sets. Default is syntax
 
-        #Common Syntax Characters
-list_ = {"syntax": 
+list_ = {
+        #Common Syntax Characters - Default
+        
+        "syntax": 
+
             [ "*","(",")","_","+",
             "{","}","|","[","]","\\",
             ":",'"',"'",";","'",
@@ -9,8 +12,10 @@ list_ = {"syntax":
             "%","#","!","~","`","^","&" 
             ],
 
-        #Common git Commands            
+        #Common git Commands          
+        #   
         "git" :
+
             ['cd', 'cd ../', 'cd ~', 'code', 'conda activate', 'cp', 'echo', 
              'git add .', 'git branch', 'git checkout', 'git clone', 'git commit -m', 
              'git config', 'git diff', 'git fetch', 'git init', 'git log', 'git log', 
@@ -21,7 +26,9 @@ list_ = {"syntax":
             ],
         
         # Common python functions
+
         "python":
+
             ['!=', '%=', '*=', '+=', '-=', '/=', '<=', '==', '>=', 
              '.append', '.discard', '.format', '.get', '.insert', 
              '.intersection', '.pop', '.remove', '.remove', '.setdefault', 
@@ -34,7 +41,9 @@ list_ = {"syntax":
             ],
         
         #Common PANDAS functionality
+
         "pandas":
+
             ['.DataFrame', '.DataFrame', '.Series', '.append', '.apply', '.astype', 
              '.columns', '.concat', '.corr', '.count', '.describe', '.describe', '.dropna', 
              '.fillna', '.groupby', '.head', '.index=', '.info', '.isnull', '.join', '.max', 
@@ -43,5 +52,88 @@ list_ = {"syntax":
              '.read_table', '.rename', '.replace', '.set_index', '.shape', '.shift', 
              '.sort_index', '.sort_values', '.std', '.tail', '.to_csv', '.to_excel', 
              '.to_json', '.to_sql', '.value_counts', 'df', 'iloc', 'loc'
+            ],
+
+        # Common SQL functionality
+
+        "sql" : 
+
+            ['(*)',');',',',';','ADD COLUMN','ALL','ALTER TABLE','AND','AVG()','BETWEEN','BIGINT','BOOLEAN',
+            'CASCADE','COUNT()','CROSS JOIN','DEFAULT','DELETE FROM','DESC','DROP TABLE IF EXISTS',
+            'EXISTS','FROM','FULL JOIN','FULL OUTER JOIN','GROUP BY','HAVING','IN','INNER JOIN','INSERT INTO',
+            'INTO','IS NOT NULL','JOIN','LEFT JOIN','LIKE','MAX()','MIN()','NATURAL JOIN','NULL','ON','OR','ORDER BY',
+            'PRIMARY KEY','REMOVE','RIGHT JOIN','SELECT','SUM()','TRUE','WHERE','[ASC]','_%'
             ]
+                # Query order of execution
+
+                        # 1. FROM and JOINs
+
+                # The FROM clause, and subsequent JOINs are first executed to determine the 
+                # total working set of data that is being queried. This includes subqueries in
+                # this clause, and can cause temporary tables to be created under the hood 
+                # containing all the columns and rows of the tables being joined.
+
+                        # 2. WHERE
+
+                # Once we have the total working set of data, the first-pass WHERE constraints 
+                # are applied to the individual rows, and rows that do not satisfy the constraint 
+                # are discarded. Each of the constraints can only access columns directly from the 
+                # tables requested in the FROM clause. Aliases in the SELECT part of the query are 
+                # not accessible in most databases since they may include expressions dependent on
+                # parts of the query that have not yet executed.
+
+                        # 3. GROUP BY
+
+                # The remaining rows after the WHERE constraints are applied are then grouped 
+                # based on common values in the column specified in the GROUP BY clause. 
+                # As a result of the grouping, there will only be as many rows as there are unique 
+                # values in that column. Implicitly, this means that you should only need to use this 
+                # when you have aggregate functions in your query.
+
+                        # 4. HAVING
+
+                # If the query has a GROUP BY clause, then the constraints in the HAVING 
+                # clause are then applied to the grouped rows, discard the grouped rows 
+                # that don't satisfy the constraint. Like the WHERE clause, aliases are also 
+                # not accessible from this step in most databases.
+
+                        # 5. SELECT
+
+                # Any expressions in the SELECT part of the query are finally computed.
+
+                        # 6. DISTINCT
+
+                # Of the remaining rows, rows with duplicate values in the column marked as DISTINCT will be discarded.
+
+                        # 7. ORDER BY
+
+                # If an order is specified by the ORDER BY clause, the rows are then sorted by the specified
+                #  data in either ascending or descending order. Since all the expressions in the SELECT
+                #   part of the query have been computed, you can reference aliases in this clause.
+
+                        # 8. LIMIT / OFFSET
+
+                # Finally, the rows that fall outside the range specified by the LIMIT and OFFSET are
+                #  discarded, leaving the final set of rows to be returned from the query.
+
+                # Conclusion
+
+                # Not every query needs to have all the parts we listed above, but a part of why SQL
+                #  is so flexible is that it allows developers and data analysts to quickly manipulate
+                #   data without having to write additional code, all just by using the above clauses.
+
+                    #Complete SELECT query
+
+                    # SELECT DISTINCT column, AGG_FUNC(column_or_expression), …
+                    # FROM mytable
+                    #     JOIN another_table
+                    #     ON mytable.column = another_table.column
+                    #     WHERE constraint_expression
+                    #     GROUP BY column
+                    #     HAVING constraint_expression
+                    #     ORDER BY column ASC/DESC
+                    #     LIMIT count OFFSET COUNT;
+        
+       
+       
         }
